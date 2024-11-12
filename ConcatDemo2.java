08be38ff36240f87d21f69ac4c5a37b734648124
ShaolinRat <csprowalps@gmail.com>
2024-11-12 01:35:49 -0500

    adding files

---
+++ ConcatDemo2.java
@@ -0,0 +1,11 @@
+package com.glab_303_3_1;
+
+public class ConcatDemo2 {
+
+	public static void main(String[] args) {
+		// This is really about the charAt() method
+		   String message = "Welcome to Java";
+		   System.out.println("The first character in the message is " + message.charAt(0));
+	}
+
+}
